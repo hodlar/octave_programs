11358a3a9ae39f04e97328e6123a2c12edc44922
Carlos Rodri­guez <carlosrdz.isd@gmail.com>
2018-03-07 11:53:21 -0600

    corrected input bug

--- take_values.m
+++ take_values.m
@@ -1,6 +1,6 @@
 function [CRVAL1, CRVAL2, CRPIX1, CRPIX2] = take_values(name)
-CRVAL1 = fitsheader("img.fit","CRVAL1");
-CRVAL2 = fitsheader("img.fit","CRVAL2");
-CRPIX1 = fitsheader("img.fit","CRPIX1");
-CRPIX2 = fitsheader("img.fit","CRPIX2");
+CRVAL1 = fitsheader(name,"CRVAL1");
+CRVAL2 = fitsheader(name,"CRVAL2");
+CRPIX1 = fitsheader(name,"CRPIX1");
+CRPIX2 = fitsheader(name,"CRPIX2");
 endfunction
