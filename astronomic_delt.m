cec6199b6898f9a7615965b0d524f5d69515ab26
Carlos Rodri­guez <carlosrdz.isd@gmail.com>
2018-04-02 17:33:29 -0500

    Adding code to convert delts

---
+++ astronomic_delt.m
@@ -0,0 +1,20 @@
+function [X_vec,Y_vec]  = astronomic_delt(name,x,y)
+[NAXIS1, NAXIS2, CRPIX1, CRPIX2, CRVAL1, CRVAL2, PC1_1, PC2_2, CD1_1, CD2_2] = take_values(name);
+
+if( length(PC1_1) == 0 )
+	CDELT1 = CD1_1;
+	CDELT2 = CD2_2;
+end
+
+if( length(CD1_1) == 0 )
+	CDELT1 = PC1_1;
+	CDELT2 = PC2_2;
+end
+
+dim1 = 0:1:NAXIS1;
+dim2 = 0:1:NAXIS2;
+
+X_vec = CRVAL1 + (x - CRPIX1) * CDELT1;
+Y_vec = CRVAL2 + (y - CRPIX2) * CDELT2;
+
+endfunction
