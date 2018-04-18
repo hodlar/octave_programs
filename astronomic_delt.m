78659108c296a22713f24758db83c01f9f3dbba4
Carlos Rodri­guez <carlosrdz.isd@gmail.com>
2018-04-18 11:58:17 -0500

    Corrected file names

--- astronomic_delt.m
+++ astronomic_delt.m
@@ -1,6 +1,6 @@
 function [X_vec,Y_vec]  = astronomic_delt(name1, name2, x,y)
-[NAXIS1, NAXIS2, CRPIX1, CRPIX2, CRVAL1_1, CRVAL2_1, PC1_1, PC2_2, CD1_1, CD2_2] = take_values(name);
-[NAXIS1_2, NAXIS2_2, CRPIX1_2, CRPIX2_2, CRVAL1_2, CRVAL2_2, PC1_12, PC2_22, CD1_12, CD2_22] = take_values(name);
+[NAXIS1, NAXIS2, CRPIX1, CRPIX2, CRVAL1_1, CRVAL2_1, PC1_1, PC2_2, CD1_1, CD2_2] = take_values(name1);
+[NAXIS1_2, NAXIS2_2, CRPIX1_2, CRPIX2_2, CRVAL1_2, CRVAL2_2, PC1_12, PC2_22, CD1_12, CD2_22] = take_values(name2);
 
 if( length(PC1_1) == 0 )
 	CDELT1 = CD1_1;
